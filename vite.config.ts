e1f51ad26b79b659fef86a01674c5d1ca8022ea2
Ardit <ardit@starlabspro.com>
2025-05-03 00:35:58 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 /// <reference types="vite/client" />
 
-import tailwindcss from '@tailwindcss/vite';
-import react from '@vitejs/plugin-react';
-import laravel from 'laravel-vite-plugin';
-import { resolve } from 'node:path';
 import { defineConfig, loadEnv } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'node:path';
 
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, process.cwd(), '');
@@ -14,38 +13,32 @@ export default defineConfig(({ mode }) => {
         plugins: [
             laravel({
                 input: ['resources/css/app.css', 'resources/js/app.tsx'],
-                ssr: 'resources/js/ssr.tsx',
                 refresh: true,
             }),
             react(),
-            tailwindcss(),
         ],
-        esbuild: {
-            jsx: 'automatic',
-        },
         resolve: {
             alias: {
                 'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
             },
-        },
-        server: {
-            hmr: isLocal ? {
-                host: 'localhost',
-            } : false,
-            host: isLocal ? 'localhost' : '0.0.0.0',
-            port: 5173,
-            strictPort: true,
-            cors: true,
         },
         build: {
             outDir: 'public/build',
             assetsDir: '',
             manifest: true,
             rollupOptions: {
+                input: 'resources/js/app.tsx',
                 output: {
                     manualChunks: undefined,
                 },
             },
+        },
+        server: {
+            host: isLocal ? 'localhost' : '0.0.0.0',
+            port: 5173,
+            cors: true,
+            strictPort: true,
+            hmr: isLocal ? { host: 'localhost' } : false,
         },
         base: isLocal ? undefined : '/build/',
     };

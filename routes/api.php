c01b263fae652d4537ab56087db9760f7f5a089e
Ardit <ardit@starlabspro.com>
2025-05-03 22:48:32 +0200

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,5 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 // Group routes that need session handling
-Route::middleware(['web'])->group(function () {
-    Route::post('/auth/login', [AuthController::class, 'login']);
-    Route::post('/auth/register', [AuthController::class, 'register']);
-});
+Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/register', [AuthController::class, 'register']);

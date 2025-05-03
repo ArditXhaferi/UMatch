937f63a887120c5a73a2242337549d9ee1080b8a
Ardit <ardit@starlabspro.com>
2025-05-03 23:25:09 +0200

    Revert "Update api.php"
    This reverts commit c01b263fae652d4537ab56087db9760f7f5a089e.

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AuthController;
 
 // Group routes that need session handling
-Route::post('/auth/login', [AuthController::class, 'login']);
-Route::post('/auth/register', [AuthController::class, 'register']);
+Route::middleware(['web'])->group(function () {
+    Route::post('/auth/login', [AuthController::class, 'login']);
+    Route::post('/auth/register', [AuthController::class, 'register']);
+});
